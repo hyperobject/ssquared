806d8a0376824b301fd3fc67106b6bed0d39a2ae
Technoboy10 <technoboy10@fake.email>
2013-04-01 14:06:45 -0600

    Updated to v1.0! Woot!

--- S^2.py
+++ S^2.py
@@ -8,35 +8,29 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	if 'stop' in path:
 		bot.stop()
 	elif 'motorinf' in path:
-		regex = re.compile("\/motor([0-9+])-([0-9]+)")
+		regex = re.compile('\/motorinf([0-9]+)-([0-9]+)([+|-])([+|-])')
 		m = regex.match(path)
-		l = int(m.group(1)) / 100
-		r = int(m.group(2)) / 100
+		if m.group(3) == '-':
+			l = int(m.group(1)) * -0.01
+		else:
+			l = int(m.group(1)) * 0.01
+		if m.group(4) == '-':
+			r = int(m.group(2)) * -0.01
+		else:
+			r = int(m.group(2)) * 0.01
 		bot.motors(l, r)
-	elif 'motortime' in path:
-		regex = re.compile("\/motortime([0-9+])-([0-9]+)-([0-9]+)")
-		m = regex.match(path)
-		l = int(m.group(1)) / 100
-		r = int(m.group(2)) / 100
-		time = int(m.group(3))
-		bot.motors(l, r)	
-		time.sleep(time)
-		bot.stop()	
-	elif 'movetime' in path:
-		regex = re.compile("\/movetime([0-9]+)-([0-9]+)")
-		m = regex.match(path)
-		power = int(m.group(1)) / 100
-		time = int(m.group(2))
-		bot.translate(power)
-		time.sleep(time)
-		bot.stop()
 	elif 'moveinf' in path:
-		regex = re.compile("\/moveinf([0-9]+)")
+		regex = re.compile("\/moveinf([0-9]+)([+|-])")
 		m = regex.match(path)
-		power = m.group(1) / 100
+		if m.group(2) == '-':
+			power = int(m.group(1)) * -0.01
+		else:
+			power = int(m.group(1)) * 0.01
+		print int(m.group(1))
+		print power
 		bot.translate(power)
 	elif 'led' in path:
-		regex = re.compile("\/led([left|right])([0-9]+)")
+		regex = re.compile("\/led([left|center|right])([on|off])")
 		m = regex.match(path)
 		led = m.group(1)
 		power = int(m.group(2))
@@ -44,26 +38,12 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	elif 'tone' in path:
 		regex = re.compile("\/tone([0-9]+)-([0-9]+)")
 		m = regex.match(path)
-		time = int(m.group(1))
+		time = int(m.group(1)) * 0.01
 		freq = int(m.group(2))
 		bot.beep(time, freq)
 	elif path == '/stall': #return data
 		f = open(ospath + '/return', 'w+')
-		f.write(bot.getStall())
-		f.close()
-		f = open(ospath + '/return', 'rb')
-		ctype = self.guess_type(ospath + '/return')
-		self.send_response(200)
-	        self.send_header("Content-type", ctype)
-	        fs = os.fstat(f.fileno())
-	        self.send_header("Content-Length", str(fs[6]))
-	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-	        self.send_header("Access-Control-Allow-Origin", "*")
-	        self.end_headers()
-	        return f
-	elif path == '/stall': #return data
-		f = open(ospath + '/return', 'w+')
-		f.write(bot.getStall())
+		f.write(str(bot.getStall()))
 		f.close()
 		f = open(ospath + '/return', 'rb')
 		ctype = self.guess_type(ospath + '/return')
@@ -89,13 +69,50 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	        self.send_header("Access-Control-Allow-Origin", "*")
 	        self.end_headers()
 	        return f
-	elif path == '/getpos': 
-		regex = re.compile("\/getpos([ir|line|light])-([0-9]+)")
+	elif 'setn' in path:
+		regex = re.compile('\/setn([a-zA-z0-9]+)')
 		m = regex.match(path)
-		sensor = m.group(1)
-		pos = int(m.group(2)) - 1
+		bot.setName(m.group(1))
+	elif 'light' in path:
+		regex = re.compile("\/light([0-9]+)")
+		m = regex.match(path)
+		pos = int(m.group(1)) - 1
 		f = open(ospath + '/return', 'w+')
-		f.write(bot.get(sensor, pos))
+		f.write(str(bot.getLight(pos)))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+	        return f
+	elif 'IR' in path:
+		regex = re.compile("\/IR([0-9]+)")
+		m = regex.match(path)
+		pos = int(m.group(1)) - 1
+		f = open(ospath + '/return', 'w+')
+		f.write(str(bot.getIR(pos)))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+	        return f
+	elif 'line' in path:
+		regex = re.compile("\/line([0-9]+)")
+		m = regex.match(path)
+		pos = int(m.group(1)) - 1
+		f = open(ospath + '/return', 'w+')
+		f.write(str(bot.getLine(pos)))
 		f.close()
 		f = open(ospath + '/return', 'rb')
 		ctype = self.guess_type(ospath + '/return')
@@ -113,7 +130,8 @@ if __name__ == "__main__":
     import SocketServer
     from myro import *
     PORT = 3610 #S^2 with a 0
-    bot = Scribbler(str(raw_input('What port is your robot at?')))
+    botport = raw_input('What port is your robot at? ')
+    bot = Scribbler(botport)
     Handler = CORSHTTPRequestHandler
     #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
